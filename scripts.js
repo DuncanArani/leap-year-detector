0268f0ce7cb2bd83ce15cace76bd194127129636
ARUNCO <duncanarani@gmail.com>
2018-07-10 16:33:03 +0300

    well

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var year = parseInt($("input#year").val());
     // console.log(year);
     var result = leapYear(year);
+    $(".year").text(year);
+
     $("#result").text(result);
 
 
@@ -11,11 +13,12 @@ $(document).ready(function() {
 });
 var leapYear = function(year) {
 
-  if (year %4===0 )&&(year % 100 !== 0)) {
-    return true;
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    $(".not").text("");
   } else {
-    return false;
+    $(".not").text("not")
   }
 
+  $("#result").show();
 
 };
